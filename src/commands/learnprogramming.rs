33a38495539852ef730031696ff2784ae8b3fd61
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-09-06 17:54:18 -0400

    Formatting fix

--- src/commands/learnprogramming.rs
+++ src/commands/learnprogramming.rs
@@ -1,4 +1,3 @@
-use super::is_in_incorrect_channel;
 use serenity::{
     client::Context,
     framework::standard::{macros::command, CommandResult},
@@ -6,8 +5,8 @@ use serenity::{
 };
 
 #[command]
-#[aliases("learn", "coding")]
-#[description("Gives users links to some wonderful programming resources.")]
+#[aliases("code", "learn")]
+#[description("Gives handy programming links")]
 #[usage("")]
 #[example = ""]
 #[help_available]
@@ -16,13 +15,12 @@ pub(crate) async fn learnprogramming(ctx: &Context, msg: &Message) -> CommandRes
     msg.channel_id
         .say(
             &ctx.http,
-"**__Learn The Basics of Programming__**
-
+            "**__Learn The Basics of Programming__**
 *These following courses explain the basics of programming, and give you a solid base.*
           
--<https://www.edx.org/course/cs50s-introduction-computer-science-harvardx-cs50x>
--<https://www.reddit.com/r/learnprogramming/comments/61oly8/new_read_me_first/>
--<https://www.reddit.com/r/learnprogramming/wiki/faq#wiki_getting_started>
+<https://www.edx.org/course/cs50s-introduction-computer-science-harvardx-cs50x>
+<https://www.reddit.com/r/learnprogramming/comments/61oly8/new_read_me_first/>
+<https://www.reddit.com/r/learnprogramming/wiki/faq#wiki_getting_started>
           
 *This server is not affiliated with these courses in any way.*",
         )

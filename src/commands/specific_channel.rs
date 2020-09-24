0212ce1d72c5617d5125a2ca0ce6dfe742a66afd
Casper <lenscas@gmail.com>
2020-09-24 12:25:08 +0200

    add rank command

--- src/commands/specific_channel.rs
+++ src/commands/specific_channel.rs
@@ -1,9 +1,11 @@
 mod github;
 mod top;
+mod me;
 
-use github::GITHUB_COMMAND;
+use github::{GITHUB_COMMAND,BUG_COMMAND};
 use serenity::framework::standard::macros::group;
 use top::TOP_COMMAND;
+use me::RANK_COMMAND;
 #[group]
-#[commands(github, top)]
+#[commands(github, top,bug,rank)]
 pub(crate) struct SpecificChannel;

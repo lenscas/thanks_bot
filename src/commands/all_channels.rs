1ea6060b4df3f116fcde7a5e046d13956301e825
Casper <lenscas@gmail.com>
2020-09-24 12:26:34 +0200

    fix thx

--- src/commands/all_channels.rs
+++ src/commands/all_channels.rs
@@ -4,11 +4,13 @@ mod codeblock;
 mod google;
 mod learnprogramming;
 mod questions;
+mod thanks;
 
 use codeblock::CODEBLOCK_COMMAND;
 use google::GOOGLE_COMMAND;
 use learnprogramming::LEARNPROGRAMMING_COMMAND;
 use questions::QUESTIONS_COMMAND;
+use thanks::THX_COMMAND;
 #[group]
-#[commands(codeblock, google, questions, learnprogramming)]
+#[commands(codeblock, google, questions, learnprogramming,thx)]
 pub(crate) struct AllChannels;
